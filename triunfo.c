c3dcef434de0bc50eed122185492430f4b30635d
Dheyson210 <annydedezinha1998@gmail.com>
2025-03-02 14:59:55 +0000

    1ºetapa

---
+++ triunfo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int carta;
+    char estado;
+    printf( "Digite sua carta: \n");
+    scanf( "%d", &carta);
+    printf( "digite seu estado: \n");
+    scanf( "%c", &estado);
+    printf( "Suas informações: %d\n, %c");
+    
+
+
+
+    
+
+   
+}
